bff266d8ad116cecc32c1b43cb511c88aff0e157
moer <wjx2699@outlook.com>
2018-06-10 18:33:49 +0800

    solve 10008

--- wechatminiprogramforbluetooth-master/pages/bluetooth/bluetooth.js
+++ wechatminiprogramforbluetooth-master/pages/bluetooth/bluetooth.js
@@ -1,6 +1,6 @@
 // pages/bluetooth/bluetooth.js
 const app = getApp()
-var connectedDeviceId
+var deviceId
 var util = require('../utils/util.js')
 
 Page({
@@ -13,10 +13,9 @@ Page({
     name: '',
     advertisData: '',
     serviceId: '',
-    connectedDeviceId: '',
+    deviceId: '',
     serviceIds: [],
-    cdE1: '',
-    characteristicsE1: null,
+    chaId: '',
     result: ''
   },
   buf2hex: function (buffer) {
@@ -40,103 +39,96 @@ Page({
     })
     that.setData({
       menu: [
-        { 'index': 0, 'item': '浇水', 'value': 90, 'deg': 0, 'src':"../../images/xuanzhuan.png"},
-        { 'index': 1, 'item': '旋转', 'value': 70, 'deg': 90, 'src': "../../images/xuanzhuan.png"},
-        { 'index': 2, 'item': '施肥', 'value': 80, 'deg': 180, 'src': "../../images/xuanzhuan.png"},
-        { 'index': 3, 'item': '旋转', 'value': 70, 'deg': 270, 'src': "../../images/xuanzhuan.png"}
+        { 'index': 0, 'item': '浇水', 'value': 90, 'deg': 0, 'src': "../../images/xuanzhuan.png", "button": "formSubmit" },
+        { 'index': 1, 'item': '旋转', 'value': 70, 'deg': 90, 'src': "../../images/xuanzhuan.png", "button": "formSubmit1" },
+        { 'index': 2, 'item': '施肥', 'value': 80, 'deg': 180, 'src': "../../images/xuanzhuan.png", "button": "formSubmit2" },
+        { 'index': 3, 'item': '旋转', 'value': 70, 'deg': 270, 'src': "../../images/xuanzhuan.png", "button": "formSubmit3" }
       ]
     })
-    connectedDeviceId = options.connectedDeviceId
     console.log('connectedDeviceId: ' + options.connectedDeviceId)
     console.log('name: ' + options.name)
     console.log('advertisData: ' + options.advertisData)
     that.setData({
       deviceconnected: true,
-      connectedDeviceId: options.connectedDeviceId,
+      deviceId: options.connectedDeviceId,
       name: options.name,
       advertisData: options.advertisData
     })
-      wx.getBLEDeviceServices({
-        deviceId: connectedDeviceId,
-        success: function (res) {
-          console.log('device services:', res.services)
-          that.setData({ serviceIds: res.services })
-          for (var i = 0; i < that.data.serviceIds.length; i++){
-            if (that.data.serviceIds[i].uuid.indexOf("FFE0") != -1) {
-              that.setData({ serviceId: that.data.serviceIds[i].uuid })
-            }
+    wx.getBLEDeviceServices({
+      deviceId: that.data.deviceId,
+      success: function (res) {
+        console.log('All device serviceIds:', res.services)
+        that.setData({ serviceIds: res.services })
+        for (var i = 0; i < that.data.serviceIds.length; i++) {
+          if (that.data.serviceIds[i].uuid.indexOf("FFE0") != -1) {
+            that.setData({ serviceId: that.data.serviceIds[i].uuid })
           }
-          console.log('--------------------------------------')
-          console.log('device设备的id:', that.data.connectedDeviceId)
-          console.log('device设备的服务id:', that.data.serviceId)
-          setTimeout(function () {
+        }
+        console.log('--------------------------------------')
+        console.log('设备的id:', that.data.deviceId)
+        console.log('设备的serviceId:', that.data.serviceId)
+        setTimeout(function () {
           wx.getBLEDeviceCharacteristics({
-            deviceId: that.data.connectedDeviceId,
+            deviceId: that.data.deviceId,
             serviceId: that.data.serviceId,
             success: function (res) {
-              console.log('device getBLEDeviceCharacteristics:', res.characteristics)
-              if (res.characteristics[0].uuid.indexOf("FFE1") != -1) {
-                that.setData({
-                  cdE1: res.characteristics[0].uuid,
-                  characteristicsE1: res.characteristics[0]
-                })
+              console.log('All device characteristics:', res.characteristics)
+              for (var i = 0; i < res.characteristics.length; i++) {
+                if (res.characteristics[i].uuid.indexOf("FFE1") != -1) {
+                  console.log('设备的characteristicsID: ' + res.characteristics[i].uuid)
+                  that.setData({
+                    chaId: res.characteristics[i].uuid
+                  })
+                }
               }
               /**
-              */
-              console.log('cdE1=', that.data.cdE1)
-              //console.log('cd01= ' + that.data.cd01 + 'cd02= ' + that.data.cd02 + 'cd03= ' + that.data.cd03 + 'cd04= ' + that.data.cd04 + 'cdE1= ' + that.data.cdE1)
-              /**
-               * 回调获取 设备发过来的数据
-               */
-              wx.onBLECharacteristicValueChange(function (characteristic) {
-                console.log('characteristic value comed:', characteristic)
-                //{value: ArrayBuffer, deviceId: "3C:A3:08:95:D0:4B", serviceId: "0000FFE0-0000-1000-8000-00805F9B34FB", characteristicId: "0000FFE1-0000-1000-8000-00805F9B34FB"}
-                /**
-                }
-                */
-                if (characteristic.characteristicId.indexOf("FFE1") != -1) {
-                  const result = characteristic.value
-                  const hex = that.buf2hex(result)
-                  console.log(hex)
-                  that.setData({ result: hex })
-                }
-              })
-              /**
-               * 顺序开发设备特征notifiy
+               * 
                */
               wx.notifyBLECharacteristicValueChange({
-                // 启用 notify 功能
-                // 这里的 deviceId 需要在上面的 getBluetoothDevices 或 onBluetoothDeviceFound 接口中获取
-                deviceId: that.data.connectedDeviceId,
+                deviceId: that.data.deviceId,
                 serviceId: that.data.serviceId,
-                characteristicId: that.data.cdE1,
+                characteristicId: that.data.chaId,
                 state: true,
                 success: function (res) {
-                  // success
                   console.log('notifyBLECharacteristicValueChanged success', res)
                 },
                 fail: function (res) {
                   // fail
                 },
                 complete: function (res) {
-                  // complete
                 }
               })
-              /**
-              */
             },
             fail: function (res) {
               console.log(res)
             }
           })
-        }, 1500);
+        }, 1500)
+        /**
+         * 回调获取 设备发过来的数据
+         */
+        wx.onBLECharacteristicValueChange(function (characteristic) {
+          console.log('硬件设备的属性: ', characteristic)
+          /**
+           * {value: ArrayBuffer, 
+           * deviceId: "3C:A3:08:95:D0:4B", 
+           * serviceId: "0000FFE0-0000-1000-8000-00805F9B34FB", 
+           * characteristicId: "0000FFE1-0000-1000-8000-00805F9B34FB"}
+           */
+          if (characteristic.characteristicId.indexOf("FFE1") != -1) {
+            const result = characteristic.value
+            const hex = that.buf2hex(result)
+            console.log('硬件发过来的数据: ' + hex)
+            that.setData({ result: hex })
+          }
+        })
       }
     })
     wx.onBluetoothAdapterStateChange(function (res) {
       console.log(res)
       if (!res.available) {
         wx.closeBLEConnection({
-          deviceId: that.data.connectedDeviceId,
+          deviceId: that.data.deviceId,
           success: function (res) {
             console.log(res)
           }
@@ -145,31 +137,94 @@ Page({
     })
   },
   formSubmit: function (e) {
-    //console.log('form发生了submit事件，携带数据为：', e.detail.value.senddata)
     console.log('发送数据：', e.currentTarget.dataset.id);
-    //var senddata = e.detail.value.senddata
-    var senddata = e.currentTarget.dataset.id
+    var sendData = e.currentTarget.dataset.id
     var that = this
-    let buffer = new ArrayBuffer(senddata.length)
+    let buffer = new ArrayBuffer(sendData.length)
     let dataView = new DataView(buffer)
-    for (var i = 0; i < senddata.length; i++) {
-      dataView.setUint8(i, senddata.charAt(i).charCodeAt())
+    for (var i = 0; i < sendData.length; i++) {
+      dataView.setUint8(i, sendData.charAt(i).charCodeAt())
     }
     wx.writeBLECharacteristicValue({
-      deviceId: that.data.connectedDeviceId,
+      deviceId: that.data.deviceId,
       serviceId: that.data.serviceId,
-      characteristicId: that.data.cdE1,
+      characteristicId: that.data.chaId,
       value: buffer,
       success: function (res) {
-        console.log(res)
         console.log('writeBLECharacteristicValue success', res.errMsg)
       },
       fail: function (res) {
         //fail
         console.log(res)
+      }
+    })
+  },
+  formSubmit1: function (e) {
+    console.log('发送数据：', e.currentTarget.dataset.id);
+    var sendData = e.currentTarget.dataset.id
+    var that = this
+    let buffer = new ArrayBuffer(sendData.length)
+    let dataView = new DataView(buffer)
+    for (var i = 0; i < sendData.length; i++) {
+      dataView.setUint8(i, sendData.charAt(i).charCodeAt())
+    }
+    wx.writeBLECharacteristicValue({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceId,
+      characteristicId: that.data.chaId,
+      value: buffer,
+      success: function (res) {
+        console.log('writeBLECharacteristicValue success', res.errMsg)
       },
-      complete: function (res) {
-        //complete
+      fail: function (res) {
+        //fail
+        console.log(res)
+      }
+    })
+  },
+  formSubmit2: function (e) {
+    console.log('发送数据：', e.currentTarget.dataset.id);
+    var sendData = e.currentTarget.dataset.id
+    var that = this
+    let buffer = new ArrayBuffer(sendData.length)
+    let dataView = new DataView(buffer)
+    for (var i = 0; i < sendData.length; i++) {
+      dataView.setUint8(i, sendData.charAt(i).charCodeAt())
+    }
+    wx.writeBLECharacteristicValue({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceId,
+      characteristicId: that.data.chaId,
+      value: buffer,
+      success: function (res) {
+        console.log('writeBLECharacteristicValue success', res.errMsg)
+      },
+      fail: function (res) {
+        //fail
+        console.log(res)
+      }
+    })
+  },
+  formSubmit3: function (e) {
+    console.log('发送数据：', e.currentTarget.dataset.id);
+    var sendData = e.currentTarget.dataset.id
+    var that = this
+    let buffer = new ArrayBuffer(sendData.length)
+    let dataView = new DataView(buffer)
+    for (var i = 0; i < sendData.length; i++) {
+      dataView.setUint8(i, sendData.charAt(i).charCodeAt())
+    }
+    wx.writeBLECharacteristicValue({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceId,
+      characteristicId: that.data.chaId,
+      value: buffer,
+      success: function (res) {
+        console.log('writeBLECharacteristicValue success', res.errMsg)
+      },
+      fail: function (res) {
+        //fail
+        console.log(res)
       }
     })
   },
@@ -181,7 +236,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
